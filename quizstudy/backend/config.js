a7532fa15a4a8958b9a5123e7f9a7f534d3dcf57
savan Kumar <121648350+ShivSaini011@users.noreply.github.com>
2024-08-13 16:48:34 +0530

    Delete quizstudy directory for new changes adding

--- quizstudy/backend/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const dotenv = require('dotenv');
-dotenv.config();
-
-module.exports = {
-  port: process.env.PORT,
-  mongoURI: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET,
-};
