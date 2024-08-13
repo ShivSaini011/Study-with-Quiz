de2682e196474d329baabd9c1f358609cdfbed44
savan Kumar <121648350+ShivSaini011@users.noreply.github.com>
2024-08-13 21:38:49 +0530

    add some funcanilty in landing page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -23,12 +23,14 @@ const Navbar  = () => {
 };
 
 
+
+
   return (
     <div>
     {/* <nav className="navbar">
       <div className="navbar-logo">Logo</div>
       <div className="navbar-menu">
-        <div className="navbar-item">
+        <div className="navbar-item"> 
           <a href="/" className="navbar-link">Home</a>
         </div>
         <div className="navbar-item">
@@ -80,6 +82,107 @@ const Navbar  = () => {
 
 
 
+<div
+        data-animation="default"
+        class="navbar_fixed w-nav"
+        data-easing2="ease"
+        fs-scrolldisable-element="smart-nav"
+        data-easing="ease"
+        data-collapse="medium"
+        data-w-id="5d852efa-8106-c00b-da3b-afd629aa8ccf"
+        role="banner"
+        data-duration="0"
+      >
+        <div class="navbar_component">
+          <div class="navbar_container">
+            <a
+              href="/"
+              aria-current="page"
+              class="navbar_logo-link w-nav-brand w--current"
+              aria-label="home"
+              ><img
+                src="/"
+                loading="lazy"
+                alt="Codewars by Andela"
+                class="navbar_logo"
+            /></a>
+            <nav role="navigation" class="navbar_menu w-nav-menu">
+            <div className="navbar_menu-links">
+                <div><a
+                  href="/study"
+                  className="navbar_link w-nav-link"
+                  >Study</a
+                ></div>
+                <div><a
+                  href="/quiz"
+                  className="navbar_link w-nav-link"
+                  >Quiz</a
+                >
+                <div className="dropdown-content">
+                <a href="/quiz/c">C</a>
+                <a href="/quiz/c++">C++</a>
+                <a href="/quiz/java">Java</a>
+                <a href="/quiz/python">Python</a>
+                </div></div>
+                <div><a
+                  href="/About"
+                  target="_blank"
+                  className="navbar_link w-nav-link"
+                  >About</a
+                ></div>
+                </div>
+              {token ? (
+          <div className="profile-container">
+            <div className="navbar-user-icon" onClick={handleProfileClick}>
+               {/* <h3>profile</h3>  */}
+              <i className="fa fa-user-circle" aria-hidden="true"></i>
+            </div>
+            {dropdownVisible && (
+              <div className="profile-dropdown">
+                <a href="/profile">Profile</a>
+                <a href="/profile/books">View Your Books</a>
+                <a href="/" onClick={handleLogout}>Logout</a>
+              </div>
+            )}
+          </div>
+        ) : (
+              <div className="navbar_menu-buttons">
+                <a
+                  id="login-btn"
+                  href="/login"
+                  className="button is-tertiary is-small w-button"
+                  >Log in</a
+                ><a
+                  id="join-btn"
+                  href="/signup"
+                  className="button is-secondary is-small w-button"
+                  >Join</a
+                >
+              </div>
+            )}
+            </nav>
+            <div
+              class="navbar_menu-button w-nav-button"
+              aria-label="menu"
+              role="button"
+              tabindex="0"
+              aria-controls="w-nav-overlay-0"
+              aria-haspopup="menu"
+              aria-expanded="false"
+            >
+              <div class="menu-icon1">
+                <div class="menu-icon1_line-top"></div>
+                <div class="menu-icon1_line-middle">
+                  <div class="menu-icon_line-middle-inner"></div>
+                </div>
+                <div class="menu-icon1_line-bottom"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-nav-overlay" data-wf-ignore="" id="w-nav-overlay-0"></div>
+      </div>
+
 
 
       <div
@@ -101,7 +204,7 @@ const Navbar  = () => {
               className="navbar_logo-link w-nav-brand w--current"
               aria-label="home"
               ><img
-                src="./Codewars - Achieve mastery through coding practice and developer mentorship_files/6674f0cdb5b7b401612cf0ee_codewars-by-andela-dark.svg"
+                src="/"
                 loading="lazy"
                 alt="Code by savan"
                 className="navbar_logo"
